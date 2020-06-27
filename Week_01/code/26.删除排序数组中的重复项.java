f5696205165849c85226da0c199962515b420af9
jaylinjiehong <jaylinjiehong@163.com>
2020-06-27 23:51:34 +0800

    26. 删除排序数组中的重复项

---
+++ Week_01/code/26.删除排序数组中的重复项.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=java
+ *
+ * [26] 删除排序数组中的重复项
+ * 题目分析：1、排序数组
+ *          2、原地删除重复元素
+ *          3、返回数组新长度
+ *          4、O(1)额外空间
+ * 解法：快慢指针
+ */
+
+// @lc code=start
+class Solution {
+    private static final int BOUNDARY_VALUE = 2;
+    public int removeDuplicates(int[] nums) {
+        if(nums==null){
+            return 0;
+        }
+
+        int length = nums.length;
+        if(length<BOUNDARY_VALUE){
+            return length;
+        }
+
+        int slowPointer = 0;
+        int quickPointer = 1;
+
+        for( ; quickPointer < length; quickPointer++){
+            if(nums[slowPointer] != nums[quickPointer]){
+                nums[++slowPointer] = nums[quickPointer];
+            }
+        }
+
+        return slowPointer+1;
+    }
+}
+// @lc code=end
+
