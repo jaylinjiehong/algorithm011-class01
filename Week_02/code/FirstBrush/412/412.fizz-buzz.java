a2a022ece095a2b39fe68af02d802777dd49d9a1
jaylinjiehong <jaylinjiehong@163.com>
2020-07-01 13:12:10 +0800

    412题的第一种解法

---
+++ Week_02/code/FirstBrush/412/412.fizz-buzz.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=412 lang=java
+ *
+ * [412] Fizz Buzz
+ * 模拟法：按照题目算法描述转化为代码
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        if(n < 0 ){
+            return null;
+        }
+        List<String> result =  new ArrayList<>();
+        int divideBy3 , divideBy5 ;
+        for(int i = 1; i <= n; i++){
+            divideBy3 = i % 3;
+            divideBy5 = i % 5;
+            if(divideBy3 == 0 && divideBy5 == 0){
+                result.add("FizzBuzz");
+                continue;
+            }
+            if(divideBy3 == 0){
+                result.add("Fizz");
+                continue;
+            }
+            if(divideBy5 == 0){
+                result.add("Buzz");
+                continue;
+            }
+            result.add(String.valueOf(i));
+        }
+        return result;
+    }
+}
+// @lc code=end
+
