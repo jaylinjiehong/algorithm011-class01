567198622029d99c1e67e598e25e75a62fdddf62
jaylinjiehong <jaylinjiehong@163.com>
2020-07-29 22:46:27 +0800

    完成213的一刷

---
+++ Week_06/code/FirstBrush/213.打家劫舍-ii.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 动态规划
+     * 数学归纳法
+     */
+    public int rob(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length-1)),myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+
+    }
+
+    private int myRob(int[] nums) {
+        int pre =0,cur =0,tmp;
+        for(int num:nums){
+            tmp = cur;
+            cur = Math.max(pre+num, cur);
+            pre = tmp;
+        }
+        return cur;
+    }
+}
+// @lc code=end
+
