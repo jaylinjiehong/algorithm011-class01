5e2e6ec53eb886965573bde09253394bc9cbadcb
jaylinjiehong <jaylinjiehong@163.com>
2020-08-13 00:16:50 +0800

    完成198的一刷

---
+++ Week_08/code/FirstBrush/198.打家劫舍.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=java
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+}
+// @lc code=end
+
